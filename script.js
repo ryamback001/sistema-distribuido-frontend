43a73223d7378b98ff889a4df7fbd6654b980f90
ryamback001 <almeida.rianba2001@gmail.com>
2026-05-04 12:48:33 -0300

    corrigindo teste

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_USUARIOS = "http://IP_DA_AZURE:3002";
 const API_SERVICOS = "https://SEU_NGROK.io";
 
 function teste() {
-    console.log("clicou no botão");
+    console.log("Botão clicado!");
 
     const resultado = document.getElementById("resultado");
     resultado.textContent = "✅ Funcionando!";
