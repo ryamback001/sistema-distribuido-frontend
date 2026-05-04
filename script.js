4876498bacd5bbbcfed404b37697400a2df5d499
ryamback001 <almeida.rianba2001@gmail.com>
2026-05-04 12:25:59 -0300

    criando front

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const API_USUARIOS = "http://IP_DA_AZURE:3002";
 const API_SERVICOS = "https://SEU_NGROK.io";
+
+function teste() {
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = "✅ Botão funcionando!";
+}
