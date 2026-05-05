5daa18a27f8c7df78b9b324ce0e52e1b9c808336
ryamback001 <almeida.rianba2001@gmail.com>
2026-05-04 22:48:14 -0300

    teste ok evoluindo front 2

--- front/script.js
+++ front/script.js
@@ -1,9 +1,34 @@
-const API_USUARIOS = "http://IP_DA_AZURE:3002";
-const API_SERVICOS = "https://SEU_NGROK.io";
+const AZURE = "http://IP_DA_AZURE:3002";
+const PC = "https://SEU_NGROK.io";
 
-function teste() {
-    console.log("Botão clicado!");
+// TESTE AZURE
+async function testarAzure() {
+    try {
+        const res = await fetch(`${AZURE}/teste`);
+        const data = await res.text();
 
-    const resultado = document.getElementById("resultado");
-    resultado.textContent = "✅ Funcionando!";
+        document.getElementById("resultado").textContent =
+            "Azure respondeu: " + data;
+
+    } catch (erro) {
+        document.getElementById("resultado").textContent =
+            "❌ Erro Azure";
+        console.error(erro);
+    }
+}
+
+// TESTE PC
+async function testarPC() {
+    try {
+        const res = await fetch(`${PC}/teste`);
+        const data = await res.text();
+
+        document.getElementById("resultado").textContent =
+            "PC respondeu: " + data;
+
+    } catch (erro) {
+        document.getElementById("resultado").textContent =
+            "❌ Erro PC";
+        console.error(erro);
+    }
 }
