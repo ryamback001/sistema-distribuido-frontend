a21b8e0c7d597e39951b5f9ecaba0d90146aa68d
ryamback001 <almeida.rianba2001@gmail.com>
2026-05-04 23:30:58 -0300

    teste azure 1

--- front/script.js
+++ front/script.js
@@ -2,21 +2,18 @@ const AZURE = "http://IP_DA_AZURE:3002";
 const PC = "https://SEU_NGROK.io";
 
 // TESTE AZURE
-async function testarAzure() {
-    try {
-        const res = await fetch(`${AZURE}/teste`);
-        const data = await res.text();
-
-        document.getElementById("resultado").textContent =
-            "Azure respondeu: " + data;
-
-    } catch (erro) {
-        document.getElementById("resultado").textContent =
-            "❌ Erro Azure";
-        console.error(erro);
-    }
+function testarConexaoAzure() {
+    fetch("http://20.151.117.60:3002/teste")
+        .then(response => response.text())
+        .then(data => {
+            console.log(data); 
+            alert("Conexão com Azure: " + data);
+        })
+        .catch(err => {
+            console.error("Erro na requisição:", err);
+            alert("Erro ao conectar com a Azure. Verifique o console.");
+        });
 }
-
 // TESTE PC
 async function testarPC() {
     try {
