3678c976017c452f4106b8e9aab59119c77be5a0
riam <almeida.rianba2001@gmail.com>
2026-06-03 06:31:53 -0300

    arumando index

--- backend-localhome/index.js
+++ backend-localhome/index.js
@@ -1,2 +1,105 @@
+const express = require('express');
+const cors = require('cors');
 
->>>>>>> 427685a (alteracao home)
+const app = express();
+const PORT = 3000;
+
+const AZURE_BASE = "http://20.151.117.60:3002";
+
+app.use(cors());
+app.use(express.json());
+
+// Cadastro
+app.post('/usuarios', async (req, res) => {
+    try {
+        const respostaAzure = await fetch(
+            `${AZURE_BASE}/usuarios`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(req.body)
+            }
+        );
+
+        const dados = await respostaAzure.json();
+
+        res.status(respostaAzure.status).json(dados);
+
+    } catch (erro) {
+
+        res.status(500).json({
+            erro: erro.message
+        });
+
+    }
+});
+
+// Login
+app.post('/login', async (req, res) => {
+
+    try {
+
+        const respostaAzure = await fetch(
+            `${AZURE_BASE}/login`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(req.body)
+            }
+        );
+
+        const dados = await respostaAzure.json();
+
+        res.status(respostaAzure.status).json(dados);
+
+    } catch (erro) {
+
+        res.status(500).json({
+            erro: erro.message
+        });
+
+    }
+});
+
+// Listagem
+app.get('/usuarios', async (req, res) => {
+
+    try {
+
+        const respostaAzure =
+            await fetch(`${AZURE_BASE}/usuarios`);
+
+        const dados =
+            await respostaAzure.json();
+
+        res.json(dados);
+
+    } catch (erro) {
+
+        res.status(500).json({
+            erro: erro.message
+        });
+
+    }
+});
+
+// Status
+app.get('/', (req, res) => {
+
+    res.json({
+        status: "HOME ONLINE"
+    });
+
+});
+
+app.listen(PORT, () => {
+
+    console.log(
+        `💻 Nó Local ativo na porta ${PORT}`
+    );
+
+});
